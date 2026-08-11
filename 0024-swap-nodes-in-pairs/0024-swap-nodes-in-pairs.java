a94ddfb1819aa50bfa339751358546ca8abc1388
Pooja <poojasreebhogavalli@gmail.com>
2026-08-11 15:31:58 +0530

    Time: 0 ms (100%), Space: 43.3 MB (12.14%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(0,head);
+        ListNode temp = dummy;
+
+        while(temp.next != null  && temp.next.next != null){
+            ListNode first = temp.next;
+            ListNode second = first.next;
+
+            first.next = second.next;
+            second.next = first;
+            temp.next = second;
+
+            temp = first;
+        }
+        return dummy.next;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
