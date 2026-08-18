c21013dc72cf55bd51e5306c51106e4eed138b70
Pooja <poojasreebhogavalli@gmail.com>
2026-08-18 10:11:14 +0530

    Time: 3 ms (41.15%), Space: 44.1 MB (22.91%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String clearDigits(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0; i<s.length();i++){
+            char ch = s.charAt(i);
+            if(!st.isEmpty() && Character.isDigit(ch)){
+                st.pop();
+            }
+
+            else{
+                st.push(ch);
+            }
+        }
+
+        StringBuilder str = new StringBuilder();
+        while(!st.isEmpty()){
+            str.append(st.pop());
+        }
+        return str.reverse().toString();
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
